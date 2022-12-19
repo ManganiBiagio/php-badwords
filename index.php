9b0ebca3ec459e057ecfe6446ae8988fb6053e49
ManganiBiagio <biagiomangani@live.it>
2022-12-19 14:03:05 +0100

    form creation

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php Badwords</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-5">
+                <form action="">
+                  <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Parola da censurare</label>
+                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="...">
+                 </div>
+                  <div class="mb-3">
+                     <label for="exampleFormControlTextarea1" class="form-label">Testo da censurare</label>
+                     <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                  </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
